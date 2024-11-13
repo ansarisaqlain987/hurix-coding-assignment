c5146bac4bc7c5003cabd4de6e20e1f5ecabcd27
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-11-13 17:04:31 +0530

    enhanced ui

--- fe-react/src/components/book-card.tsx
+++ fe-react/src/components/book-card.tsx
@@ -16,37 +16,43 @@ export const BookCard: FC<PropsWithChildren<{ book: VolumeItems }>> = ({
 }) => {
   return (
     <Card className="flex flex-col overflow-hidden hover:shadow-2xl transition-shadow duration-300">
-      <div className="relative h-64 w-full justify-center">
+      <div className="flex h-64 w-full justify-center p-4">
         <img
           src={book.volumeInfo.imageLinks?.thumbnail}
           alt={`Cover of ${book.volumeInfo.title}`}
           className="transition-transform duration-300 hover:scale-105"
+          height={200}
+          width={180}
         />
       </div>
-      <CardHeader className="flex-grow">
+      <CardHeader className="flex-grow  text-left">
         <div className="flex justify-between items-start flex-col">
           <div>
-            <CardTitle className="text-xl font-semibold text-gray-800">
+            <CardTitle className="text-xl font-semibold ">
               {book.volumeInfo.title}
             </CardTitle>
-            <CardDescription className="text-sm text-gray-600">
+            <CardDescription className="text-sm ">
               by {book.volumeInfo.authors?.join(", ")}
             </CardDescription>
           </div>
-          <Badge variant="secondary" className="text-xs">
-            {book.volumeInfo.categories?.join(", ")}
-          </Badge>
         </div>
       </CardHeader>
       <CardContent>
-        <p className="text-gray-700 line-clamp-3">
-          {book.volumeInfo.description}
-        </p>
-        <div className="flex items-center mt-4 text-sm text-gray-600">
-          <Star className="w-4 h-4 text-yellow-500 mr-1" />
-          <span className="mr-4">{book.volumeInfo.averageRating}</span>
-          <BookOpen className="w-4 h-4 mr-1" />
-          <span>{book.volumeInfo.pageCount} pages</span>
+        <div>
+          <p className="text-gray-700 line-clamp-3 text-justify">
+            {book.volumeInfo.description}
+          </p>
+          <div className="flex items-center mt-4 text-sm text-gray-600">
+            <Star className="w-4 h-4 text-yellow-500 mr-1" />
+            <span className="mr-4">{book.volumeInfo.averageRating}</span>
+            <BookOpen className="w-4 h-4 mr-1" />
+            <span>{book.volumeInfo.pageCount} pages</span>
+          </div>
+        </div>
+        <div className="text-left mt-4">
+          <Badge variant="secondary" className="text-xs">
+            {book.volumeInfo.categories?.join(", ")}
+          </Badge>
         </div>
       </CardContent>
       <CardFooter className="flex justify-end bg-gray-50">
